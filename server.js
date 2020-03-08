d89ece44895354822cfcd7e5f970498e413b744d
unknown <harbouchedjamel48@gmail.com>
2020-03-08 18:20:21 +0100

    chichi

--- server.js
+++ server.js
@@ -59,3 +59,4 @@ app.get('/crud/:location',(req, res) => main.getTableLocation(req, res, db));
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT || 3000}`)
   });
+  
